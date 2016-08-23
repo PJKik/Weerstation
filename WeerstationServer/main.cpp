0fb0ce52631dba252713b390f4c34e6e8d1fd1f4
PJKik <p.j.kik@hotmail.com>
2016-08-23 14:18:12 +0200

    Project setup

--- WeerstationServer/main.cpp
+++ WeerstationServer/main.cpp
@@ -1,7 +1,14 @@
+/************************************************************************/
+/*                      main.cpp                                        */
+/*                                                                      */
+/*  Startpunt van de module WeerstationServer.                          */
+/*                                                                      */
+/*  V0.1   23-08-2008   P.J.Kik   Initiele versie                       */
+/************************************************************************/
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	printf("hello world testje\n");
 	return 0;
 }
